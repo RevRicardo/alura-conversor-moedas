05e0001303d49c481fed69bde2f2c20a7536e6af
RevRicardo <revricardolacerda@gmail.com>
2025-05-06 05:41:54 -0300

    Inicia projeto

---
+++ src/ConversorMoedasApplication.java
@@ -0,0 +1,13 @@
+public class ConversorMoedasApplication {
+    public static void main(String[] args) {
+
+        String azul = "\u001B[96m";
+        String reset = "\u001B[0m";
+        System.out.println(azul + """
+                *---------------------------------*
+                *           ALURA - ONE           *
+                *   Sistema Conversor de Moedas   *
+                *---------------------------------*
+                """ + reset);
+    }
+}
