977ba014ec4798bbff89321dfd386a7a65d3c6cd
RevRicardo <revricardolacerda@gmail.com>
2025-05-06 17:00:39 -0300

    Implementa verificação opção digitada para conversão

--- src/ConversorMoedasApplication.java
+++ src/ConversorMoedasApplication.java
@@ -1,11 +1,13 @@
 import service.MoedaConversao;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConversorMoedasApplication {
     public static void main(String[] args) throws IOException {
         String azul = "\033[34m";
+        String vermelhoNegrito = "\033[1;31m";
         String reset = "\033[0m";
 
         Scanner scanner = new Scanner(System.in);
@@ -31,14 +33,20 @@ public class ConversorMoedasApplication {
             System.out.print("Opção: ");
             opcao = scanner.nextInt();
 
+            if (opcao < 1 || opcao > 8) {
+                System.out.println(vermelhoNegrito + "Opção inválida. Verifique o menu e digite um código entre 1 e 8." + reset);
+                System.out.println();
+                continue;
+            }
+
             if (opcao != 8) {
                 System.out.print("Digite o valor a ser convertido: ");
-                double valor = scanner.nextDouble();
-                processarConversao(opcao, valor);
+                double valor = scanner.nextDouble(); processarConversao(opcao, valor);
                 System.out.println("\nPressione Enter para continuar...");
                 scanner.nextLine();
                 scanner.nextLine();
             }
+
         } while (opcao != 8);
 
         System.out.println("Aplicação encerrada.");
